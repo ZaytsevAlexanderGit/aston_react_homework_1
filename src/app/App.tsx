5c2194b1c99ae4320d3972aa25f0145899843faf
Alexander Zaytsev <sanyageologist@mail.ru>
2025-07-02 09:11:38 +0300

    Refactor after PR#1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,12 @@ import './App.css';
 import { ErrorBoundary } from '../features/error-boundary/ErrorBoundary.tsx';
 import { MainLayout } from '../shared/MainLayout.tsx';
 import { PostList } from '../widgets';
-import { defaultPostsData } from '../widgets/PostList/constants.ts';
 
 function App() {
   return (
     <ErrorBoundary>
       <MainLayout>
-        <PostList posts={defaultPostsData} />
+        <PostList />
       </MainLayout>
     </ErrorBoundary>
   );
